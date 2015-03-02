6439d307e592538713b503585f27c2c13e957be2
Eduardo Quadros <quadros.ea@gmail.com>
2015-03-02 03:30:51 -0300

    Initial Commit

---
+++ Rendezvous/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rendezvous
+//
+//  Created by Eduardo Quadros on 3/2/15.
+//  Copyright (c) 2015 Eduardo Quadros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Rendezvous/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rendezvous
+//
+//  Created by Eduardo Quadros on 3/2/15.
+//  Copyright (c) 2015 Eduardo Quadros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
